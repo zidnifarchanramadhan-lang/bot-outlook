ff06723831d6b6710de9c4d6754bf6077dce9279
zayse <zidnifarchanramadhan@gmail.com>
2026-08-30 14:11:44 +0700

    Extend webhook timeout for browser tasks

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,6 +1,10 @@
 import { webhookCallback } from "grammy";
 import { bot } from "../src/bot.js";
 
-// Vercel Serverless function handler
-export default webhookCallback(bot, "http");
+// Vercel Serverless function handler with extended timeout for browser automation
+export default webhookCallback(bot, "http", {
+  onTimeout: "return",
+  timeoutMilliseconds: 58000,
+});
+
 
